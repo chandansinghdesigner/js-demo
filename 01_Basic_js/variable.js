60b9847b5eb1acb4243f029120647c136e5bb478
chandansinghdesigner <32226098+chandansinghdesigner@users.noreply.github.com>
2025-11-16 03:38:32 +0000

    val is changes

---
+++ 01_Basic_js/variable.js
@@ -0,0 +1,21 @@
+const accountId = 1114;
+
+let   accountEmail = "singhchandan.singh3@gmailcom";
+var   accountPassw = '12345';
+var accountCity = "Ara";
+let accountState;
+
+//accountId = 2;
+// this is not allowd const;
+console.log(accountId);
+accountEmail = "CHandan@gmail.com";
+accountPassw = "5356428736492"
+accountCity = "Tarari"
+
+
+console.table ([accountId, accountEmail, accountPassw, accountCity, accountState]);
+
+/* 
+ Can not prefer to var 
+
+*/
