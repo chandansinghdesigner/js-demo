f860e83c86f4a658bbcd819389c2b460f792ede7
chandansinghdesigner <32226098+chandansinghdesigner@users.noreply.github.com>
2025-11-16 14:22:31 +0000

    Use type of dataType

---
+++ 01_Basic_js/datatype.js
@@ -0,0 +1,41 @@
+"use strict" // tehret all java script new virsion
+
+console.log( 3 + 4);
+
+//alert.log ( "this is addition", 3 + 4); // we  are used node js not a brawser.
+
+console.log(67 + 
+    3, "Chandan kumar singh"
+
+
+
+); // Code readbility shude be heigh
+
+
+// console.log(age);
+// console.log(typeof age);
+
+
+let name = "chandan kr singh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
+
+
+
